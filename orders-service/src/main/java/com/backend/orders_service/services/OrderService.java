d519c4568209321da9c8ffb084f048da10e642a4
Exe <exelcarrizo@gmail.com>
2024-12-17 19:58:01 -0300

    Implements WebFlux and BaseResponse

--- orders-service/src/main/java/com/backend/orders_service/services/OrderService.java
+++ orders-service/src/main/java/com/backend/orders_service/services/OrderService.java
@@ -2,11 +2,13 @@ package com.backend.orders_service.services;
 
 import com.backend.orders_service.models.Order;
 import com.backend.orders_service.models.OrderItems;
+import com.backend.orders_service.models.dto.BaseResponse;
 import com.backend.orders_service.models.dto.OrderItemRequest;
 import com.backend.orders_service.models.dto.OrderRequest;
 import com.backend.orders_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -17,18 +19,30 @@ public class OrderService {
 
     private  final OrderRepository orderRepository;
 
+    private final WebClient.Builder webClientBuilder;
+
     public void placeOrder(OrderRequest orderRequest){
 
         //Check for inventory
+        BaseResponse result =this.webClientBuilder.build()
+                .post()
+                .uri("http//:localhost:8083/api/inventory/in-stock")
+                .bodyValue(orderRequest.getOrderItems())
+                .retrieve()
+                .bodyToMono(BaseResponse.class)
+                .block();
 
+        if (result != null && !result.hasErrors()) {
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setOrderItems(orderRequest.getOrderItems().stream()
-                .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
-                .collect(Collectors.toList()));
-        this.orderRepository.save(order);
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setOrderItems(orderRequest.getOrderItems().stream()
+                    .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
+                    .toList());
 
+        }else {
+            throw new IllegalArgumentException("Some of the products are not in stock");
+        }
     }
 
     private Order mapOrderItemRequestToOrderItem(OrderItemRequest orderItemRequest, Order order){

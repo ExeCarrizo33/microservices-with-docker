41d17400caa6a3233f9ebe23e79df2fbf6bc48b9
Exe <exelcarrizo@gmail.com>
2024-12-17 18:41:31 -0300

    Microservice finished products

--- products-service/src/main/java/com/backend/products_service/models/Product.java
+++ products-service/src/main/java/com/backend/products_service/models/Product.java
@@ -1,17 +1,15 @@
 package com.backend.products_service.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
-@NoArgsConstructor
+@Table(name = "product")
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
-@Table(name="product")
 public class Product {
 
     @Id
